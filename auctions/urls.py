6e2d9ac98cfb517dde27452363c839e9d0bcf332
Danypooh <delatorrechapell@gmail.com>
2024-08-10 11:34:03 -0600

    list active listings on index page & add style

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -9,3 +12,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("new_listing", views.new_listing, name="new_listing"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
